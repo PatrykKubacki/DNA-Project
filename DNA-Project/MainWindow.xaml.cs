763f4d575961f92da1881c1814d131f4d3f23089
patryk.kubacki@outlook.com <patryk.kubacki@outlook.com>
2017-04-06 15:27:11 +0200

    NEW: ProgressBar

--- DNA-Project/MainWindow.xaml.cs
+++ DNA-Project/MainWindow.xaml.cs
@@ -22,39 +22,52 @@ namespace DNA_Project
             _sequenceSize = 2;
         }
 
-         void Button_Click(object sender, RoutedEventArgs e)
+         async void Button_Click(object sender, RoutedEventArgs e)
          {
+            ProgressBar.Visibility = Visibility.Visible;
             var genomSize = Int32.Parse(GenomSizeLabel.Text);
             _dna = new Dna(genomSize);
-            GenomLabel.Text = string.Empty;
-            foreach (var x in _dna.Genom)
+            GenomLabel.Text = await GetGenom();
+            ProgressBar.Visibility = Visibility.Hidden;
+        }
+
+        async Task<string> GetGenom()
+        {
+            return await Task.Run(() =>
             {
-                GenomLabel.Text += x.ToString();
-            }
+                return _dna.Genom.Aggregate(string.Empty, (current, x) => current + x.ToString());
+            });
         }
 
-        void Button_Click_1(object sender, RoutedEventArgs e)
+        async void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            var result = Search(_dna.PreciseSearch);
+            ProgressBar.Visibility = Visibility.Visible;
+            var result = await Search(_dna.PreciseSearch, GenomSizeLabel.Text);
             PreciseSearchResultLabel.Content = result;
+            ProgressBar.Visibility = Visibility.Hidden;
         }
 
-        void Button_Click_2(object sender, RoutedEventArgs e)
+        async void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            var result = Search(_dna.OptimalSearch);
+            ProgressBar.Visibility = Visibility.Visible;
+            var result = await Search(_dna.OptimalSearch, GenomSizeLabel.Text);
             OptimalSearchResultLabel.Content = result;
+            ProgressBar.Visibility = Visibility.Hidden;
         }
 
-        string Search(SearchDelegate search)
+        async Task<string> Search(SearchDelegate search, string genomSize)
         {
-            var size = _sequenceSize;
-            _dna.SetSequence(size);
-            _timer.Start();
-            var result = search();
-            double seconds = _timer.Elapsed.TotalSeconds;
-            _timer.Reset();
-            MessageBox.Show($"Znaleziono {result}\n w czasie: {seconds} sekund", "Rezultat przeszukiwania", MessageBoxButton.OK, MessageBoxImage.Information);
-            return $"Rozmiar genomu: {GenomSizeLabel.Text}\nRozmiar sekwencji: {size}\nZnaleziono: {result}\nCzas: {seconds} sekund";
+            return await Task.Run(() =>
+            {
+                var size = _sequenceSize;
+                _dna.SetSequence(size);
+                _timer.Start();
+                var result = search();
+                double seconds = _timer.Elapsed.TotalSeconds;
+                _timer.Reset();
+                MessageBox.Show($"Znaleziono {result}\n w czasie: {seconds} sekund", "Rezultat przeszukiwania", MessageBoxButton.OK, MessageBoxImage.Information);
+                return $"Rozmiar genomu: {genomSize}\nRozmiar sekwencji: {size}\nZnaleziono: {result}\nCzas: {seconds} sekund";
+            });
         }
 
         void ToggleButton1_OnChecked(object sender, RoutedEventArgs e)
